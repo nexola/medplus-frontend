8ef829b7bdef72dd4c4c0b8b75e7e1ecd9c80f34
nexola <vitormatheusfv@gmail.com>
2024-04-18 20:28:39 -0300

    Displaying input validation errors

--- script.js
+++ script.js
@@ -43,6 +43,9 @@ const formDoctor = document.querySelectorAll(".container--form-doctor .input");
 const formPatient = document.querySelectorAll(
   ".container--form-patient .input"
 );
+const containerFormDoctor = document.querySelector(".container--form-doctor");
+const containerFormPatient = document.querySelector(".container--form-patient");
+const containerFormLogin = document.querySelector(".container--form-login");
 const btnLogin = document.getElementById("btn--login");
 const btnSignupDoctor = document.getElementById("btn--doctor");
 const btnSignupPatient = document.getElementById("btn--patient");
@@ -54,9 +57,23 @@ async function postJson(endpoint, options = {}, objInput = {}) {
     : (options.body = `grant_type=password&username=${objInput.email}&password=${objInput.password}`);
   const response = await fetch(`${url}/${endpoint}`, options);
   if (!response.ok) {
-    const message = await response.text();
-    console.log(message);
-    return message;
+    const error = await response.json();
+
+    switch (endpoint) {
+      case "oauth2/token":
+        displayErrLogin("Usuário ou senha inválidos");
+        break;
+      case "doctors":
+        displayErrForm("Campos inválidos", containerFormDoctor, error);
+        break;
+      case "patients":
+        displayErrForm("Campos inválidos", containerFormPatient, error);
+      default:
+        break;
+    }
+
+    console.log(error);
+    return error;
   }
   const json = await response.json();
   if (
@@ -88,6 +105,26 @@ function clearInputs(form) {
 
 function instantFormat(string) {
   return string + "T00:00:00Z";
+}
+
+function displayErrLogin(message) {
+  const err = document.createElement("p");
+  err.classList.add("err");
+  err.textContent = message;
+  containerFormLogin.appendChild(err);
+}
+
+function displayErrForm(message, container, error) {
+  document.querySelectorAll(".err").forEach((err) => err.remove());
+  const err = document.createElement("p");
+  err.classList.add("err");
+  err.textContent = message;
+  container.appendChild(err);
+  error.errors.forEach((err) => {
+    const field = document.querySelector(`input[name=${err.field}]`);
+    field.classList.add("inputErr");
+    field.placeholder = err.message;
+  });
 }
 
 // Event Listeners
